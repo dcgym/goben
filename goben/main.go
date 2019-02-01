c8c0ef78509ac8b836a1984db45827b84af2942c
Tom Yang <tomatoyang729@gmail.com>
2019-01-31 17:19:21 -0800

    fix -- using ascii instead of adding extra option

--- goben/main.go
+++ goben/main.go
@@ -27,7 +27,6 @@ type config struct {
 	opt            options
 	passiveClient  bool // suppress client send
 	udp            bool
-	disableChart   bool // disable chart rendering
 	chart          string
 	export         string
 	csv            string
@@ -70,18 +69,17 @@ func main() {
 	flag.StringVar(&app.defaultPort, "defaultPort", ":8080", "default port")
 	flag.IntVar(&app.connections, "connections", 1, "number of parallel connections")
 	flag.StringVar(&app.reportInterval, "reportInterval", "2s", "periodic report interval\nunspecified time unit defaults to second")
-	flag.StringVar(&app.totalDuration, "totalDuration", "10s", "test total duration\nunspecified time unit defaults to second\ninf means unlimited time, and when it's set to inf, disableChart is set to true")
+	flag.StringVar(&app.totalDuration, "totalDuration", "10s", "test total duration\nunspecified time unit defaults to second\ninf means unlimited time, and when it's set to inf, no ascii chart will be rendered")
 	flag.IntVar(&app.opt.ReadSize, "readSize", 50000, "read buffer size in bytes")
 	flag.IntVar(&app.opt.WriteSize, "writeSize", 50000, "write buffer size in bytes")
 	flag.BoolVar(&app.passiveClient, "passiveClient", false, "suppress client writes")
 	flag.BoolVar(&app.opt.PassiveServer, "passiveServer", false, "suppress server writes")
 	flag.Float64Var(&app.opt.MaxSpeed, "maxSpeed", 0, "bandwidth limit in mbps (0 means unlimited)")
 	flag.BoolVar(&app.udp, "udp", false, "run client in UDP mode")
-	flag.BoolVar(&app.disableChart, "disableChart", false, "disable chart rendering\nwhen totalDuration is inf, this will always be set to true")
 	flag.StringVar(&app.chart, "chart", "", "output filename for rendering chart on client\n'%d' is parallel connection index to host\n'%s' is hostname:port\nexample: -chart chart-%d-%s.png")
 	flag.StringVar(&app.export, "export", "", "output filename for YAML exporting test results on client\n'%d' is parallel connection index to host\n'%s' is hostname:port\nexample: -export export-%d-%s.yaml")
 	flag.StringVar(&app.csv, "csv", "", "output filename for CSV exporting test results on client\n'%d' is parallel connection index to host\n'%s' is hostname:port\nexample: -csv export-%d-%s.csv")
-	flag.BoolVar(&app.ascii, "ascii", true, "plot ascii chart")
+	flag.BoolVar(&app.ascii, "ascii", true, "plot ascii chart\nthis will automatically set to false when totalDuration is inf")
 	flag.BoolVar(&app.silent, "silent", false, "Do not print any output")
 	flag.StringVar(&app.tlsKey, "key", "key.pem", "TLS key file")
 	flag.StringVar(&app.tlsCert, "cert", "cert.pem", "TLS cert file")
@@ -105,10 +103,10 @@ func main() {
 	}
 
 	app.reportInterval = defaultTimeUnit(app.reportInterval)
-	
+
 	// when the user want to generate the traffic for unlimited time, disable chart rendering to avoid memory overflow
 	if app.totalDuration == "inf" {
-		app.disableChart = true
+		app.ascii = false
 	}
 	app.totalDuration = defaultTimeUnit(app.totalDuration)
 
